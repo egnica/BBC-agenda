dc1c54b9bca361af799d17c4a0f6b45cc991a461
egnica <46502133+egnica@users.noreply.github.com>
2025-02-03 09:30:08 -0600

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +21,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <header></header>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Image
+          className={styles.profilePic}
+          height={100}
+          width={100}
+          src="https://www.barlowresearch.com/BusinessBankingConference_2025/images/logo2.png"
+          alt={`barlow logo`}
+        />
+        <nav className={styles.nav}>
+          <a href="/">
+            <p>Conference Home</p>
+          </a>
+          <a href="/speakers">
+            <p>Speaker</p>
+          </a>
+          <a href="default.asp">
+            <p>Home</p>
+          </a>
+          <a href="default.asp">
+            <p>Home</p>
+          </a>
+        </nav>
         {children}
       </body>
     </html>
