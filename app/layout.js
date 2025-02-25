293c7ee7f0b8563fe5ea4bd94fe091b1d337cf60
egnica <46502133+egnica@users.noreply.github.com>
2025-02-25 10:57:25 -0600

    font and logo change

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fira_Sans } from "next/font/google";
 import "./globals.css";
 import styles from "./page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const firaSans = Fira_Sans({
   subsets: ["latin"],
+  weight: ["400", "700"], // Choose the weights you need
+  variable: "--font-fira-sans",
+  display: "swap", // Ensures better performance
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "2025 Barlow Conference Agenda",
   description: "Barlow Conference Agenda Chicago 2025",
@@ -22,38 +18,38 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${firaSans.variable} `}>
         <nav className={styles.nav}>
           <Link href="https://www.barlowresearch.com" passHref>
             <Image
               className={styles.logo}
-              height={100}
-              width={100}
-              src="https://mybarlow.barlowresearch.com/BusinessBankingConference_2025/images/logo2.png"
+              height={50}
+              width={180}
+              src="https://mybarlow.barlowresearch.com/BusinessBankingConference_2025/images/barlow-row-blk-logo.png"
               alt={`barlow logo`}
             />
           </Link>
 
           <Link href="https://barlowresearch.com/?page_id=9421">
-            <p>Home</p>
+            <p className={styles.link}>Home</p>
           </Link>
           <Link href="/">
-            <p>Agenda</p>
+            <p className={styles.link}>Agenda</p>
           </Link>
           {/* <Link href="/speakers">
-            <p>Speaker</p>
+            <p className={styles.link}>Speaker</p>
           </Link> */}
           <Link href="https://barlowresearch.com/?page_id=9421#venue">
-            <p>Venue</p>
+            <p className={styles.link}>Venue</p>
           </Link>
           <Link href="https://barlowresearch.com/?page_id=9421#hotel">
-            <p>Hotels</p>
+            <p className={styles.link}>Hotels</p>
           </Link>
           <Link href="https://barlowresearch.com/?page_id=9421#faq">
             <p>FAQ</p>
           </Link>
           <Link href="https://barlowresearch.com/?page_id=9421#sponsors">
-            <p>Sponsors</p>
+            <p className={styles.link}>Sponsors</p>
           </Link>
         </nav>
         {children}
